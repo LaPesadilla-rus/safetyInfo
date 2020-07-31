9125db70aaa263bd13ef349aa2ef2dd93eb54de5
BadPenguiN <fox94944@gmail.com>
2020-07-31 15:31:14 +0300

    база данных мк2

--- backend/models/main.js
+++ backend/models/main.js
@@ -4,23 +4,34 @@ const pool = new Pool (conn.conn_saf);
 const pool_bahos = new Pool (conn.conn_bahos);
 
 exports.all =  async (cb) => {
-    await pool.query(`SELECT ma.ma_id, ma.ma_prim1, ma.ma_pers_id, ma.ma_prim2 pc_name, pc_inv_num, og_name, og_recv, kg_dgvr, kg_kol, ps_ver, ps_serial, ps_name
-                FROM main_table ma
-
-                inner join ktr_dgvr kg
-                on kg.kg_id = ma_ktr_id
-
-                inner join spr_org og
-                on og.og_id = kg.kg_org_id
-
-                inner join spr_pc pc
-                on pc.pc_id = ma.ma_pc_id
-
-                inner join spr_po_skzi ps
-                on ps.ps_id = ma.ma_skzi_id
-
-                inner join spr_inf_sys ins
-                on ins.ins_id = ma.ma_infs_id
+    await pool.query(`SELECT ss.ss_name as skzi_name, ins.ins_name as inf_name, 
+                        sk.sk_ver as skzi_ver, sk.sk_serial as skzi_ser, 
+                        kt.kt_name as ktr_name, kg.kg_dgvr, kg.kg_kol, 
+                        kg.kg_arch , pc.pc_name, og.og_name as org_name, 
+                        io.*
+                        
+                        FROM info_safe io
+                        
+                        inner join spr_pc pc
+                        on pc.pc_id = io.io_pc_id
+                        
+                        inner join spr_org og
+                        on og.og_id = io.io_org_id
+                        
+                        inner join kontragents kg
+                        on kg.kg_id = io.io_ktr_id
+                        
+                        inner join spr_ktr kt
+                        on kt.kt_id = kg.kg_name_id
+                        
+                        inner join skzi sk
+                        on sk.sk_id = kg.kg_skzi_id
+                        
+                        inner join spr_skzi ss
+                        on ss.ss_id = sk.sk_name_id
+                        
+                        inner join spr_inf_sys ins
+                        on ins.ins_id = sk_inf_id 
         `).then( res => {
             cb('',res);
         }).catch( err => {
@@ -34,7 +45,7 @@ exports.pers =  async (data,cb) => {
     inner join otd ot
     on ot.ot_id = pe.pe_otd_id
 
-    WHERE pe_id = `+data.ma_pers_id+`
+    WHERE pe_id = `+data.io_pers_id+`
         `).then( res => {
             cb('',res);
         }).catch( err => {
